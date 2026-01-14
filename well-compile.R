eab4b26995478e7d3e6b053081fdff999ab2bd90
Reynolds <Reynolds.Melanie@epa.gov>
2026-01-14 14:45:45 -0800

    only well data now

--- well-compile.R
+++ well-compile.R
@@ -11,6 +11,7 @@ library(readr)
 library(tidygeocoder)
 install.packages("cdlTools")
 library(cdlTools)
+library(janitor)
 
 setwd("C:/Users/mreyno04/OneDrive - Environmental Protection Agency (EPA)/Profile/REPOS/dw-nitrate")
 
@@ -19,7 +20,6 @@ setwd("C:/Users/mreyno04/OneDrive - Environmental Protection Agency (EPA)/Profil
 # wells <- read_delim("C:/Users/mreyno04/OneDrive - Environmental Protection Agency (EPA)/Profile/REPOS/dw-nitrate/well_logs.txt")
 # 
 # welp <- wells |>
-#   drop_na(latitude, longitude) |>
 #   drop_na(use_domestic)
 # 
 # welp <- welp |>
@@ -154,22 +154,21 @@ well_county <- domes_well |>
             AM_wells.1990 = sum(wells_1990_num)) 
 
 rdc_mini <- rdc_county |>
-  select(month_date_yyyymm, county_fips, county, 
-         active_listing_count, pending_listing_count, total_listing_count,
-         quality_flag) |>
-  mutate(year = str_sub(as.character(month_date_yyyymm), 1 , 4))|>
+  select(month_date_yyyymm, county_fips, county, pending_listing_count,
+         active_listing_count, quality_flag) |>
+  mutate(year = str_sub(as.character(month_date_yyyymm),1,4))|>
   relocate(year) |>
   select(-month_date_yyyymm)
 
 rdc_mini <- rdc_mini |>
   group_by(county_fips, year) |>
-  summarise(active_listing = sum(active_listing_count),
-            pending_listing = sum(pending_listing_count),
-            total_listing = sum(total_listing_count)) |>
+  summarise(pending_listing = sum(pending_listing_count),
+            active_listing = sum(active_listing_count)) |>
   pivot_wider(id_cols = county_fips,
               names_from = year,
-              values_from = c(active_listing, pending_listing, total_listing),
-              values_fill = 0)
+              values_from = c(pending_listing, active_listing),
+              values_fill = 0) |>
+  mutate(across(everything(), ~replace_na(.x, 0)))
 
 rdc_mini <- rdc_mini |>
   left_join(well_county, by = 'county_fips')
@@ -187,217 +186,40 @@ RET_county <- RET_county |>
 
 county_all <- RET_county |>
   left_join(rdc_mini, by = 'county') |>
-  relocate(county_fips)
-
-county_all <- county_all |>
+  relocate(county_fips) |>
   drop_na(county)
 
-# cyanotoxin transport =========================================================
+# housing unit information
 
-# pws_inv <- read.csv('O:/Public/Pennino/HABs/PWS_inventory_gwsw_2023Q4.csv')
-# pws_gw <- pws_inv |>
-#   filter(str_detect(Primary.Source, regex("ground", ignore_case = TRUE)))
+housing_2024 <- readxl::read_excel('CO-EST2024-HU-41.xlsx', col_names = TRUE, skip = 1)
 
-ucmr4 <- read.csv('O:/Public/Pennino/HABs/ucmr_4_main.csv')
-ucmr_GU <- ucmr4 |>
-  filter(facility_water_type == 'GU') |>
-  #filter(contaminant == 'total microcystin' | contaminant == 'anatoxin-a' | contaminant == 'cylindrospermopsin') |>
-  rename(PWSID = pws_id)
-# ucrm_gw data frame represents GWUDI systems that have cyanotoxin presence detected
-# must combined with some sort of geographic information 
+housing_2024 <- housing_2024 |>
+  mutate(County = str_remove(County, "^\\."),
+         County = str_remove(County, " County, Oregon")) |>
+  rename(county = County)
 
-# geographic information
-# PWS_FAC_to_HUC12.csv: has duplicates due to facilities for a single PWS being in multiple HUC12s
-pws_huc <- read_csv("PWS_FAC_Locations_to_HUC12_correct.csv", col_types = cols(HUC12 = col_character()))
+housing_2020 <- readxl::read_excel('co-est2020int-hu-41.xlsx', col_names = TRUE, skip = 1)
 
-# check how many overlap 
-length(intersect(pws_huc$PWSID, ucmr_cyano$PWSID))
+housing_2020 <- housing_2020 |>
+  mutate(county = str_remove(county, "^\\."),
+         county = str_remove(county, " County, Oregon")) |>
+  rename_with(~ paste0("hu_", .), -c(county, base_2010))
 
-# join
-geo_cyanotox <- inner_join(pws_huc, ucmr_cyano, by = 'PWSID', relationship = 'many-to-many')
-# add geospatial
-nhd <- st_read("O:/LAB/COR/Geospatial_Library_Resource/Physical/HYDROLOGY/WBD/WBD_National_GDB.gdb", layer = 'WBDHU12')
+housing_2010 <- readxl::read_excel('hu-est00int-02-41.xls', col_names = TRUE)
 
-#check overlap 
-length(intersect(nhd$huc12, geo_cyanotox$HUC12))
+housing_2010 <- housing_2010 |>
+  mutate(county = str_remove(county, "^\\."),
+         county = str_remove(county, " County")) |>
+  rename_with(~ paste0("hu_", .), -county)
 
-nhd <- nhd |>
-  rename(HUC12 = huc12)
+housing <- housing_2010 |>
+  left_join(housing_2020, by = 'county') |>
+  left_join(housing_2024, by = 'county') |>
+  drop_na()
 
-# geo_all == ucmr data filtered to cyanotoxins and GWUDI then joined with HUC12 geospatial data using Micheal's
-# conversion methods and NHD huc info. Represents 94 PWS systems 
-geo_all <- nhd |>
-  inner_join(geo_cyanotox, by = 'HUC12', relationship = 'many-to-many')
+# joining 
 
-states <- tigris::states(cb = TRUE, progress_bar = FALSE)  %>%
-  filter(!STUSPS %in% c('HI', 'PR', 'AK', 'MP', 'GU', 'AS', 'VI'))  %>%
-  st_transform(crs = 5072)
-
-geo_all <- geo_all |>
-  st_transform(crs = 5072)
-
-geo_all <- geo_all |>
-  st_point_on_surface()
-
-ggplot() +
-  geom_sf(data = geo_all, fill = NA) + 
-  geom_sf(data = states, fill = NA, color = "black", lwd = 0.1) +
-  geom_sf(data = geo_combo)
-  
-ggplot(geo_combo, aes(color = pred_cyano_fit)) +
-  geom_sf()
-
-geo_combo <- st_filter(PredData, geo_all, .predicate = st_within)
-is_within <- as.vector(geo_combo)
-
-length(PredData[is_within, ])
-
-# idaho deq data
- 
-idaho <- read_csv('EDMS_2025.csv')
-sw_deq <- st_read('IDEQ_SWA_DELINEATIONS')
-
-# wisconsin shallow gw database
-
-wisc <- st_read("C:/Users/mreyno04/OneDrive - Environmental Protection Agency (EPA)/Profile/REPOS/dw-nitrate/WISC_gwdb/Private Well - PFAS Shallow Groundwater Study Results.gdb", layer = 'PW_PFAS_Study_Sample_Data')
-
-# # cyanotoxin contam
-# 
-# source <- read.csv('gwudi_sw.csv')
-# 
-# source_geo <- source_all |>
-#   geocode(address, method = 'osm', lat = latitude, long = longitude)
-# 
-# source_geo <- source_geo |>
-#   drop_na(latitude, longitude) |>
-#   st_as_sf(coords = c("longitude", "latitude"), crs = 4269) 
-#   
-# ggplot(source_geo, aes(color = Primary.Source)) +
-#   geom_sf(size = 1)
-# 
-# 
-# welp_fil <- welp |>
-#   drop_na(street, city, zip, state)
-# 
-# welp_geo <- welp_fil |>
-#   unite(col = 'address', street, city, state, zip, sep = ', ')
-# 
-# welp_geo <- welp_geo |>
-#   geocode(address, method = 'osm', lat = latitude, long = longitude)
-
-# Its happening ================================================================
-
-# If I want to compile all these PWS types I might have to pull from 
-# individual states that have the information :( 
-
-# Loading and cleaning data
-
-water_watch <- read_csv('state_data/waterwatch-gwudi.csv')
-water_watch <- water_watch |>
-  select(-starts_with("...")) |>
-  rename(PWSID = `Water System No.`,
-         County = `Principal County Served`,
-         Source = `Primary Source Water Type`,
-         PWS_name = `Water System Name`)
-
-wv <- readxl::read_excel('state_data/WV-gwudi.xlsx')
-wv <- wv |>
-  rename(PWSID = `Water System ID`,
-         County = `Principal County/Parish`,
-         Source = `Primary Water Source Type`,
-         Type = `Water System Type`,
-         PWS_name = `Water System Name`) |>
-  select(PWSID, PWS_name, Type, Status, County, Source)
-
-or <- read_csv('state_data/OR-gwudi.csv')
-or <- or |>
-  rename(PWSID = `PWS ID`,
-         PWS_name = `PWS Name\n\t\t\t\t\n`,
-         Type = `System Type`,
-         County = `County Served`,
-         Source = `Primary Source`) |>
-  select(PWSID, PWS_name, Type, Status, County, Source) |>
-  mutate(PWSID = paste0("OR", PWSID))
-
-va <- readxl::read_excel('state_data/VA-gwudi.xlsx')
-va <- va |>
-  rename(PWSID = `Water System ID`,
-         PWS_name = `Water System Name`,
-         Type = `Water System Type`,
-         County = `Principal County/Parish`,
-         Source = `Primary Water Source Type`) |>
-  select(PWSID, PWS_name, Type, Status, County, Source)
-  
-oh <- readxl::read_excel('state_data/OH-gwudi.xlsx')
-oh <- oh |>
-  rename(PWSID = `Water System ID`,
-         PWS_name = `Water System Name`,
-         Type = `Water System Type`,
-         County = `Principal County/Parish`,
-         Source = `Primary Water Source Type`) |>
-  select(PWSID, PWS_name, Type, Status, County, Source)
-  
-ms <- readxl::read_excel('state_data/MS-gwudi.xlsx')
-ms <- ms |>
-  rename(PWSID = `Water System ID`,
-         PWS_name = `Water System Name`,
-         Type = `Water System Type`,
-         County = `Principal County/Parish`,
-         Source = `Primary Water Source Type`) |>
-  select(PWSID, PWS_name, Type, Status, County, Source)
-
-
-state_in <- readxl::read_excel('state_data/IN-gwudi.xlsx')
-state_in <- state_in |>
-  rename(PWSID = `Water System ID`,
-         PWS_name = `Water System Name`,
-         Type = `Water System Type`,
-         County = `Principal County/Parish`,
-         Source = `Primary Water Source Type`) |>
-  select(PWSID, PWS_name, Type, Status, County, Source)
-
-CO <- read_csv('state_data/CO-gwudi.csv')
-CO <- CO |>
-  rename(PWSID = `PWS ID (Links to Records)`,
-         PWS_name = Name,
-         Type = `Federal Type`,
-         Source = `State Source Type`) |>
-  select(PWSID, PWS_name, Type, Status, County, Source)
-
-CA <- read_csv('state_data/CA-gwudi.csv')
-CA <- CA |>
-  rename(PWSID = `Water System No.`,
-         PWS_name = `Water System Name`,
-         Source = `Primary Source Water Type`,
-         County = `Principal County Served`) |>
-  select(PWSID, PWS_name, Type, County, Source)
-
-gwudi_all <- water_watch |>
-  bind_rows(wv, or, va, oh, ms, state_in, CO, CA)
-
-umcr_GU_sample <- ucmr_GU |>
-  rename(PWS_name = pws_name,
-         Source = facility_water_type) |>
-  select(PWSID, PWS_name, Source) |>
-  distinct()
-
-gwudi_all <- gwudi_all |>
-  bind_rows(umcr_GU_sample)
-
-# check how many overlap 
-length(intersect(pws_huc$PWSID, gwudi_all$PWSID))
-
-# join
-geo_gwudi <- inner_join(pws_huc, gwudi_all, by = 'PWSID', relationship = "many-to-many")
-
-# geo_all == ucmr data filtered to cyanotoxins and GWUDI then joined with HUC12 geospatial data using Micheal's
-# conversion methods and NHD huc info. Represents 94 PWS systems 
-geo_gwudi <- nhd |>
-  inner_join(geo_gwudi, by = 'HUC12', relationship = 'many-to-many')
-
-ggplot() +
-  geom_sf(data = geo_gwudi, fill = NA) + 
-  geom_sf(data = states, fill = NA, color = "black", lwd = 0.1) 
-
-
+county_all <- county_all |>
+  left_join(housing, by = 'county') |>
+  select(!(contains('active')))
 
