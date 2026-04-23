441533235d405b6f22b890120890706fa7260738
Reynolds <Reynolds.Melanie@epa.gov>
2026-04-23 15:02:28 -0700

    nitrate updates largest source

--- gwma.mama.R
+++ gwma.mama.R
@@ -90,8 +90,9 @@ danger <- rev(RColorBrewer::brewer.pal(4, "Spectral"))
 n_geo |>
   drop_na(nitrate) |>
   ggplot() +
-  geom_sf(aes(color = disc_nitrate), size = 2) +
-  scale_color_manual(values = danger) +
+  geom_sf(aes(color = disc_nitrate), size = 2.5) +
+  scale_color_manual(values = danger,
+                     name = 'Measured Nitrate \nConcentration') +
   geom_sf(data = gwma.shp, fill = NA, color = "black", lwd = 0.1)
 
 # print gwma
@@ -99,7 +100,45 @@ n_geo |>
 # lasar_ids <- paste(gwma_missing$`DEQ LASAR ID`, collapse = ",")
 # cat(lasar_ids)
 
+# largest source ===============================================================
 
+lub_shp <- st_read("C:/Users/mreyno04/OneDrive - Environmental Protection Agency (EPA)/Profile/REPOS/dw-nitrate/UmatillaIsotope/UmatillaIsotope/lubcomid.shp")
+
+# test <- get_nhdplus(AOI = lub_shp)
+# # Get COMIDs using nhdplusTools package
+# lub_shp$COMID<- NA
+# for (i in 1:nrow(lub_shp)){
+#   print (i)
+#   lub_shp[i,'comid'] <- nhdplusTools::discover_nhdplus_id(lub_shp[i,c('geometry')])
+# }
+
+lub_shp <- lub_shp |>
+  rename(COMID = FEATUREID)
+
+get_mets <- function(coms){
+  StreamCatTools::sc_get_data(metric = 'n_ff_2017,n_lw_2017,n_hw_2017,n_uf_2017,n_cf_2017,n_dep_2017,n_leg_2017,n_ags_2017',
+                              aoi='ws',
+                              comid = coms,
+                              showAreaSqKm = TRUE)
+}
+
+chunks <- split(lub_shp$COMID, ceiling(seq_along(lub_shp$COMID) / 50))
+
+metrics <- as.data.frame(do.call(rbind, lapply(chunks, get_mets)))
+
+# make ha 
+# divide by ha
+
+all_year <- all_year |>
+  rowwise() |>
+  mutate(n_ff_Ws = n_ff_Ws / WSAREAHA,
+         n_lw_Ws = n_lw_Ws / WSAREAHA,
+         n_hw_Ws = n_hw_Ws / WSAREAHA,
+         n_uf_Ws = n_uf_Ws / WSAREAHA,
+         n_cf_Ws = n_cf_Ws / WSAREAHA,
+         n_dep_Ws = n_dep_Ws / WSAREAHA,
+         n_leg_Ws = n_legWs / WSAREAHA,
+         n_ags_Ws = n_ags_Ws / WSAREAHA) 
 
 
 
